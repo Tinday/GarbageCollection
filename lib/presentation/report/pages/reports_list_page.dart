935b1bdc6190203c7cf754b3e7b7eb0a435b6ceb
Byron Kimani <byronkimani@gmail.com>
2023-09-27 21:52:25 +0300

    chore: reprts list
    - reports list

--- lib/presentation/report/pages/reports_list_page.dart
+++ lib/presentation/report/pages/reports_list_page.dart
@@ -4,6 +4,7 @@ import 'package:garbage_control/application/redux/actions/fetch_reports_action.d
 import 'package:garbage_control/application/redux/states/app_state.dart';
 import 'package:garbage_control/application/redux/view_models/bottom_app_state_view_model.dart';
 import 'package:garbage_control/application/redux/view_models/reports_view_model.dart';
+import 'package:garbage_control/constants/theme.dart';
 import 'package:garbage_control/presentation/core/widgets/custom_appbar.dart';
 
 class ReportsListPage extends StatefulWidget {
@@ -29,26 +30,71 @@ class _ReportsListPageState extends State<ReportsListPage> {
           converter: (Store<AppState> store) =>
               ReportsViewModel.fromStore(store),
           onInit: (store) {
-            store.dispatch(FetchReports(
-              onError: (error) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text('An error occurered when fetching reports'),
-                  ),
-                );
-              },
-              context: context,
-            ));
+            store.dispatch(
+              FetchReports(
+                onError: (error) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('An error occurred when fetching reports'),
+                    ),
+                  );
+                },
+                context: context,
+              ),
+            );
           },
           builder: (BuildContext context, ReportsViewModel vm) {
             if (vm.reports?.isNotEmpty ?? false) {
-              return ListView.builder(
-                itemCount: vm.reports?.length,
-                itemBuilder: (BuildContext context, int index) {},
+              return Column(
+                children: [
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('Send to my email'),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: vm.reports?.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          margin: const EdgeInsets.only(bottom: 12),
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 24, horizontal: 10),
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.grey),
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Column(
+                            children: <Widget>[
+                              Row(
+                                children: [
+                                  const Text(
+                                    "Area of reporting/dumping: ",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: accentColor),
+                                  ),
+                                  Text(
+                                    vm.reports?[index]?.addressOfDumping ?? '',
+                                  ),
+                                  const Spacer(),
+                                  const Icon(Icons.chevron_right)
+                                ],
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
               );
             } else {
               return Center(
-                  child: Text('You have not filed any reports so far'));
+                child: Text('You have not filed any reports so far'),
+              );
             }
           },
         ),
